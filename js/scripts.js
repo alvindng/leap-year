064808357c2b3cd50c2bb51cd62771154cb76b0d
Alvin Ng and Ryan Streur <alvin@>
2016-06-08 08:55:04 -0700

    Meet the first specification - the javascript function isLeap returns false when the user inputs a year that is not a leap year.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-  
+  var isLeap = function(year){
+    return false;
+  };
+
+  $("#input").submit(function(event){
+    var yearInput = $("#year").val();
+    leapYN = isLeap(yearInput);
+    $(".year").text(yearInput);
+    if(leapYN === true){
+    }
+    else{
+      $(".not").text("NOT")
+    }
+    event.preventDefault();
+  });
+
 })
